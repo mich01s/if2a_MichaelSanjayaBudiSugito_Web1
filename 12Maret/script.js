f6e5f1a56749e6cbc78a3181cd93c2a136bbaf84
michael sanjaya budi sugito <michaelsbs0123@gmail.com>
2026-03-12 14:35:45 +0700

    commit

---
+++ 12Maret/script.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+function myfunction(){
+    document.getElementById("demo").innerHTML = "Paragraph change"
+    document.getElementById("demo").style.display="flex"
+}
+function myfunction1(){
+    var x = 3;
+    let y = 5;
+    const z = x+y;
+    console.log(z)
+}
